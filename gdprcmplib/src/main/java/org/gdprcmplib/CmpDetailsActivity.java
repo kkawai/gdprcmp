67d45dc78c8882d44f1ea54dd0fae9d25bfd4723
kkawai <kkawai2@gmail.com>
2018-05-14 09:23:03 -0700

    more

--- gdprcmplib/src/main/java/org/gdprcmplib/CmpDetailsActivity.java
+++ gdprcmplib/src/main/java/org/gdprcmplib/CmpDetailsActivity.java
@@ -3,9 +3,14 @@ package org.gdprcmplib;
 import android.databinding.DataBindingUtil;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import org.json.JSONObject;
@@ -14,11 +19,12 @@ public class CmpDetailsActivity extends AppCompatActivity {
 
     public static final String TAG = "CmpDetailsActivity";
     private GdprData data;
-
+    private RecyclerView recyclerView;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        DataBindingUtil.setContentView(this, R.layout.gdpr_detailed_layout);
+        setContentView(R.layout.gdpr_detailed_layout);
+        recyclerView = findViewById(R.id.recyclerView);
         new AsyncTask<Void,Void,GdprData>() {
             @Override
             protected GdprData doInBackground(Void... voids) {
@@ -33,6 +39,11 @@ public class CmpDetailsActivity extends AppCompatActivity {
 
             @Override
             protected void onPostExecute(GdprData gdprData) {
+                if (gdprData == null) {
+                    //handle error
+                    return;
+                }
+                CmpDetailsActivity.this.data = gdprData;
                 /*try {
                     textView.setText("Vendor list size: " + GdprData.VENDORS.size() + " lastUpdated: "+gdprData.getLastUpdated());
                 }catch (Exception e) {
@@ -40,6 +51,37 @@ public class CmpDetailsActivity extends AppCompatActivity {
                 }*/
             }
         }.execute();
+    }
+
+    private class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    }
+
+    private class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView name, descr, featuresAndPurposes;
+        CheckBox checkbox;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.name);
+            descr = itemView.findViewById(R.id.descr);
+            featuresAndPurposes = itemView.findViewById(R.id.features_and_purposes);
+            checkbox = itemView.findViewById(R.id.checkbox);
+        }
     }
 
     public void onSave(View view) {
