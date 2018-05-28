c1c2cda645b6250be19043c2fcb551e7476609fa
kkawai <kkawai2@gmail.com>
2018-05-27 23:00:49 -0700

    added string translations!  increased cmp version.  1.0.5

--- gdprcmplib/src/main/java/org/gdprcmplib/Config.java
+++ gdprcmplib/src/main/java/org/gdprcmplib/Config.java
@@ -4,7 +4,7 @@ final class Config {
     static final boolean IS_LOGGING_ENABLED = true;
     static final String VENDOR_LIST_URL = "https://vendorlist.consensu.org/vendorlist.json";
     static final int CMP_ID = 1; //todo get real
-    static final int CMP_VERSION = 1; //todo increment whenever you do release
+    static final int CMP_VERSION = 2; //todo increment whenever you do release
     static final int CMP_SCREEN_ID_1 = 1;
     static final int CMP_SCREEN_ID_2 = 2;
     static final String DEFAULT_CMP_LANGUAGE = "EN";
